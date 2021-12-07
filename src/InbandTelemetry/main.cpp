fae2e44525cfb5fdd02f0420ce9ef36cf95461f0
monishaprasad4 <monisha.r.prasad@gmail.com>
2021-12-06 22:47:21 -0700

    Call functions to calculate Euler circuit and print it to stdout

--- src/InbandTelemetry/main.cpp
+++ src/InbandTelemetry/main.cpp
@@ -1,35 +1,54 @@
-// InbandTelemetry.cpp : 
-// This file contains the 'main' function. Program execution begins and ends there.
-//
+/** main.cpp
+ *  Main entry point into the program
+ *
+ *  Creates a graph object and reads it from stdin
+ *  Finds the various properties of the graph and
+ *  prints them to stdout
+ *
+ */
 
 #include <iostream>
 #include <stdio.h>
-#include "graph.h"
+#include "Graph.h"
 #include "LinkedList.h"
 
-using namespace std;
-
-
+/** The entry point into the program. 
+ *    1. Creates the graph object with input from stdin
+ *    2. Generates the various properties such as shortest path
+ *       between vertices, find odd degree vertices, perfect
+ *       matching, and Euler circuit
+ *    3. Prints the results
+ */
 int main()
 {
     Graph* graph = new Graph();
-    const char* inputFile = "campus.txt";
-
-    bool retValue = graph->LoadGraph(inputFile);
-
-    if (!retValue)
+    
+    if (!graph->ReadGraph())
     {
-        cout << "Unable to initialize graph from input file " <<
-            inputFile << ". Please check if the file exists" << endl;
-
         return -1;
     }
 
+    // Generate the various properties of the graph
+    graph->FindShortestPathBetweenVertices();
+
+    graph->FindOddDegreeVertices();
+
+    graph->FindShortestPathForOddDegreeVertices();
+
+    graph->FindPerfectMatching();
+
+    graph->InsertVirtualEdgesIntoGraph();
+
+    graph->FindEulerCircuit();
+
+    // Print out the results
     graph->PrintOddVertices();
 
     graph->PrintShortestPathOddVertices();
 
+    graph->PrintGreedyPerfetcMatchingVertices();
+
+    graph->PrintEulerCircuit();
+
     return 0;
 }
-
-
