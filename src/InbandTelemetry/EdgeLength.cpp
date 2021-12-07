31cc2fc295424c41284eaf9a14fb4b4f652c99e7
monishaprasad4 <monisha.r.prasad@gmail.com>
2021-12-06 22:45:12 -0700

    Tracks edge length lists

--- src/InbandTelemetry/EdgeLength.cpp
+++ src/InbandTelemetry/EdgeLength.cpp
@@ -1,1 +1,20 @@
+/** EdgeLength.cpp
+ *  Implementation for the EdgeLength class
+ *
+ *  Maintains a list of edges of the same length in the graph.
+ *  Used to find matching set using the Greedy Algorithm
+ */
+
+#include <iostream>
 #include "EdgeLength.h"
+
+/** Output operator for cout
+ *
+ *  Outputs the instance variables
+ */
+std::ostream& operator<<(std::ostream& os, const EdgeLength& edgeLength)
+{
+    os << "Length: " << edgeLength.length;
+
+    return os;
+}
--- src/InbandTelemetry/EdgeLength.h
+++ src/InbandTelemetry/EdgeLength.h
@@ -1,5 +1,20 @@
+/** EdgeLength.h
+ *  Definition of the EdgeLength class
+ */
+
 #pragma once
+
+#include <iostream>
+#include "LinkedList.h"
+#include "Edge.h"
+
 class EdgeLength
 {
+public:
+
+    LinkedList<Edge>* edgesList;
+
+    int length;
+    friend std::ostream& operator<< (std::ostream& os, const EdgeLength& edgeLength);
 };
 
