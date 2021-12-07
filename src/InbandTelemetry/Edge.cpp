97fa22f6c74ac8bce0f5e258afad99373dceb123
monishaprasad4 <monisha.r.prasad@gmail.com>
2021-12-06 22:44:54 -0700

    Stores data for an edge in the graph

--- src/InbandTelemetry/Edge.cpp
+++ src/InbandTelemetry/Edge.cpp
@@ -1,7 +1,63 @@
+/** Edge.cpp
+ *  Implementation for the Edge class
+ *
+ *  Encapsulates information for an edge in the graph
+ *  including the vertices, weight and whether this is 
+ *  a virtual edge or not
+ */
+
 #include "Edge.h"
 
+ /** Constructor
+  *
+  *  Initializes instance variables
+  */
 Edge::Edge()
 {
-	vertex1 = 0;
-	vertex2 = 0;
+    vertex1 = 0;
+    vertex2 = 0;
+    weight = 0;
+    isVirtual = false;
+}
+
+/** Constructor
+ *
+ *  Initializes instance variables
+ * 
+ *  @param v1 The first vertex in the edge
+ *  @param v2 The second vertex in the edge
+ */
+Edge::Edge(int v1, int v2)
+{
+    vertex1 = v1;
+    vertex2 = v2;
+    this->weight = 1;
+    isVirtual = false;
+}
+
+/** Constructor
+ *
+ *  Initializes instance variables
+ *
+ *  @param v1 The first vertex in the edge
+ *  @param v2 The second vertex in the edge
+ *  @param weight Weight of the edge
+ */
+Edge::Edge(int v1, int v2, int weight)
+{
+    vertex1 = v1;
+    vertex2 = v2;
+    this->weight = weight;
+    isVirtual = false;
+}
+
+/** Output operator for cout
+ *
+ *  Outputs the instance variables
+ */
+std::ostream& operator<<(std::ostream& os, const Edge& edge)
+{
+    os << "(" << edge.vertex1 << "," << edge.vertex2 << ")";
+
+    return os;
 }
--- src/InbandTelemetry/Edge.h
+++ src/InbandTelemetry/Edge.h
@@ -1,24 +1,37 @@
+/** Edge.h
+ *  Definition of the Edge class
+ */
+
 #pragma once
+
+#include<iostream>
+
 class Edge
 {
 public:
-	int vertex1;
-	int vertex2;
+    int vertex1;
+    int vertex2;
+    int weight;
+    bool isVirtual;
 
-	Edge();
+    Edge();
+    Edge(int vertex1, int vertex2);
+    Edge(int vertex1, int vertex2, int weight);
 
-	bool operator<(const Edge& other) const
-	{
-		if (vertex1 < other.vertex1)
-		{
-			return true;
-		}
-		else if (vertex1 == other.vertex1 && vertex2 < other.vertex2)
-		{
-			return true;
-		}
+    bool operator<(const Edge& other) const
+    {
+        if (vertex1 < other.vertex1)
+        {
+            return true;
+        }
+        else if (vertex1 == other.vertex1 && vertex2 < other.vertex2)
+        {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
+
+    friend std::ostream& operator<< (std::ostream& os, const Edge& edge);
 };
 
