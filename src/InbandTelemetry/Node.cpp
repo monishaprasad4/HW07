8a51df68a811d96c4339a10e1b952d8801d0a7d8
monishaprasad4 <monisha.r.prasad@gmail.com>
2021-12-06 22:46:27 -0700

    Added << operator for cout

--- src/InbandTelemetry/Node.cpp
+++ src/InbandTelemetry/Node.cpp
@@ -1,11 +1,24 @@
-#include "Node.h"
-#include <cstddef>
+/** Node.cpp
+ *  Implementation for the Node class
+ *
+ *  This is a template class for a node in the linked list
+ */
 
-template <class T>
-Node<T>::Node()
+#include <iostream>
+#include "Node.h"
+#include "EdgeLength.h"
+
+ /** Output operator for cout
+  *
+  *  Outputs the instance variables
+  */
+template<class T>
+std::ostream& operator<<(std::ostream& os, const Node<T>& node)
 {
-	val = 0;
-	next = NULL;
+    os << node.val;
+            
+    return os;
 }
 
 template class Node<int>;
+template class Node<EdgeLength>;
--- src/InbandTelemetry/Node.h
+++ src/InbandTelemetry/Node.h
@@ -1,11 +1,26 @@
+/** Node.h
+ *  Definition of the Node class
+ */
+
 #pragma once
+
+#include <iostream>
 
 template <class T>
 class Node
 {
 public:
-	Node();
-	T val;
-	Node<T>* next;
+    Node(T value)
+    {
+        next = NULL;
+        val = value;
+    }
+
+    T val;
+
+    Node<T>* next;
+
+    template <class U> friend std::ostream& operator<< (std::ostream& os, const Node<U>& node);
+
 };
 
